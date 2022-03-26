dc1e55c4e63f80e4cb626df95238f9719cfd0397
Stephanie Ranegar <stephanie.ranegar@gmail.com>
2022-03-26 18:34:37 -0400

    Adding JQuery slick carousel to Reviews subsection to infintely loop through reviews

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-let shop = document.getElementsByClassName("home-cta");
-
+//slick slider logic
+$('.slider').slick({
+    adaptiveHeight: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    pauseOnDotsHover: true
+});
